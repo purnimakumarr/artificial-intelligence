59a2a5f4f917e9ac17b632a73f397b42cc2f9057
Purnima Kumar <purnimakumar2021@gmail.com>
2022-01-21 12:43:55 +0530

    Update fibonacci.pl

--- 04-Fibonacci/fibonacci.pl
+++ 04-Fibonacci/fibonacci.pl
@@ -12,7 +12,7 @@ generate_fib(1, 1).
 generate_fib(N, T):- N1 is N - 1,
   % T1 = (N-1)th term of the fibonacci series
   generate_fib(N1, T1),
-  % T2 = (N-2)th term of the fibonacci series
   N2 is N - 2,
+  % T2 = (N-2)th term of the fibonacci series
   generate_fib(N2, T2),
   T is T1 + T2.
